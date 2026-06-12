5ed0d7ed8211eb931a4625be25fc4521e54fc632
Teoh Yi Le <yileteoh@gmail.com>
2026-06-12 15:58:02 +0800

    Fix backend bug

--- backend/routes/buddyRoutes.js
+++ backend/routes/buddyRoutes.js
@@ -7,7 +7,7 @@ router.get('/status', checkBuddyStatus);
 router.post('/request', sendBuddyRequest);
 router.put('/accept', acceptBuddyRequest);
 router.post('/remove', removeBuddy);
-router.get('/pending/:userId', getPendingRequests);
-router.get('/buddy/:userId', getMyBuddyProfile);
+router.get('/requests/:userId', getPendingRequests);
+router.get('/mybuddy/:userId', getMyBuddyProfile);
 
 module.exports = router;
